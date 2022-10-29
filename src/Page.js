e5377e2058fd04e935df1592d7be8bc9602b77c7
Davideo12 <j.davideo12@gmail.com>
2022-10-29 08:28:37 -0500

    second commit

--- src/Page.js
+++ src/Page.js
@@ -3,11 +3,11 @@ const DB = require("./DbManager")
 require('dotenv').config()
 
 const dbConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DATABASE
+    host: new String(process.env.DB_HOST),
+    user: new String(process.env.DB_USER),
+    password: new String(process.env.DB_PASSWORD),
+    port: new Number(process.env.DB_PORT),
+    database: new String(process.env.DATABASE)
 }
 
 
